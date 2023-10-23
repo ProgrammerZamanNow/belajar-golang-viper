fb23c79e8d32d1791a52d6ca292e8950426c8de8
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-23 21:14:04 +0700

    environment variable

--- viper_test.go
+++ viper_test.go
@@ -61,3 +61,22 @@ func TestENVFile(t *testing.T) {
 	assert.Equal(t, 3306, config.GetInt("DATABASE_PORT"))
 	assert.Equal(t, true, config.GetBool("DATABASE_SHOW_SQL"))
 }
+
+func TestENV(t *testing.T) {
+	config := viper.New()
+	config.SetConfigFile("config.env")
+	config.AddConfigPath(".")
+	config.AutomaticEnv()
+
+	// read config
+	err := config.ReadInConfig()
+	assert.Nil(t, err)
+
+	assert.Equal(t, "belajar-golang-viper", config.GetString("APP_NAME"))
+	assert.Equal(t, "Eko Kurniawan Khannedy", config.GetString("APP_AUTHOR"))
+	assert.Equal(t, "localhost", config.GetString("DATABASE_HOST"))
+	assert.Equal(t, 3306, config.GetInt("DATABASE_PORT"))
+	assert.Equal(t, true, config.GetBool("DATABASE_SHOW_SQL"))
+
+	assert.Equal(t, "Hello", config.GetString("FROM_ENV"))
+}
