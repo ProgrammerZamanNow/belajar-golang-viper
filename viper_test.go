d1aad225992c9ecc54c7e0e35ffcc63983de96c1
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-23 20:55:09 +0700

    create project viper

---
+++ viper_test.go
@@ -0,0 +1,12 @@
+package belajar_golang_viper
+
+import (
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestViper(t *testing.T) {
+	var config *viper.Viper = viper.New()
+	assert.NotNil(t, config)
+}
