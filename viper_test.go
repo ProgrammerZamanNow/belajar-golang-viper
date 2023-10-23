f297620de62836b9c75b08b3a94dce86f48e24bb
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-23 21:03:37 +0700

    json

--- viper_test.go
+++ viper_test.go
@@ -10,3 +10,20 @@ func TestViper(t *testing.T) {
 	var config *viper.Viper = viper.New()
 	assert.NotNil(t, config)
 }
+
+func TestJSON(t *testing.T) {
+	config := viper.New()
+	config.SetConfigName("config")
+	config.SetConfigType("json")
+	config.AddConfigPath(".")
+
+	// read config
+	err := config.ReadInConfig()
+	assert.Nil(t, err)
+
+	assert.Equal(t, "belajar-golang-viper", config.GetString("app.name"))
+	assert.Equal(t, "Eko Kurniawan Khannedy", config.GetString("app.author"))
+	assert.Equal(t, "localhost", config.GetString("database.host"))
+	assert.Equal(t, 3306, config.GetInt("database.port"))
+	assert.Equal(t, true, config.GetBool("database.show_sql"))
+}
